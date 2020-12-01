741f2e7ee07503ae0a85de9f37b553e9e61c83b3
Maxime <maxime.schlegel.83@gmail.com>
2020-12-01 14:56:38 +0100

    move main to own file

---
+++ code/src/tunnel.c
@@ -0,0 +1,39 @@
+#include "iftun.h"
+#include "extreminte.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    char* configScriptPath = NULL;
+    char* tunName = NULL;
+    char* output = NULL;
+
+    int opt = 1;
+    for (; opt < argc - 2; opt++) {
+        if (argv[opt][0] == '-') {
+            switch (argv[opt][1]) {
+            case 'c':
+                configScriptPath = argv[opt + 1];
+                opt++;
+                break;
+            case 'o':
+                output = argv[opt + 1];
+                opt++;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-c /configuration script path/] [-o /output file path/] /tun name/\n", argv[0]);
+                return -1;
+            }
+        } else {
+            fprintf(stderr, "Usage: %s [-c /configuration script path/] [-o /output file path/] /tun name/\n", argv[0]);
+            return -1;
+        }
+    }
+    tunName = argv[opt];
+
+    if (output) {
+        int outputFd = open(argv[3], O_WRONLY | O_CREAT);
+        return iftun( tunName, configScriptPath, outputFd);
+    }
+    return iftun( tunName, configScriptPath, 1);
+}
