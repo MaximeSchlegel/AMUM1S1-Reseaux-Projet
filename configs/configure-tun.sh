884f02ee4b7d26a353492c634b418447ea0880d7
Maxime <maxime.schlegel.83@gmail.com>
2020-12-08 21:58:42 +0100

    pensait l'avoir save

--- configs/configure-tun.sh
+++ configs/configure-tun.sh
@@ -5,8 +5,8 @@
 
 
 ip -6 link set $1 up
-ip -6 addr add $2 dev $1
+ip -6 addr add $2"/64" dev $1
 
 sysctl -q -w net.ipv6.conf.all.forwarding=1
 
-ip -6 route add $3 via $2 dev $1
+ip -6 route add $3 via $2"0" dev $1
